3376842d23a0d2dcd3f57bf90b5ad73e424653e6
Turtlepaw <turtlesimsyt@gmail.com>
2022-04-18 13:08:02 -0400

    Update add_message.js

--- context_menus/add_message.js
+++ context_menus/add_message.js
@@ -15,7 +15,7 @@ module.exports = {
      * @param {Client} client
      */
     async execute(interaction, client){
-        if(!interaction.memberPermissions.has("ADMINISTRATOR")) return errorMessage("`❌` Invalid permissions", interaction);
+        if(!interaction.member.permissions.has("ADMINISTRATOR")) return errorMessage("`❌` Invalid permissions", interaction);
         const message = interaction.options.getMessage("message");
 
         await client.kv.set(message.id, {
