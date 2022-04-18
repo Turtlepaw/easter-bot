1e31329a38914f9f46a6a721e8ff0ebd0c68d29f
Turtlepaw <turtlesimsyt@gmail.com>
2022-04-18 13:05:24 -0400

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -26,7 +26,7 @@ module.exports = {
      * @param {Client} client
      */
     async execute(interaction, client){
-        if(!interaction.memberPermissions.has("ADMINISTRATOR") && interaction.user.id != "820465204411236362") return errorMessage("`❌` Invalid permissions", interaction);
+        if(!interaction.memberPermissions.has("ADMINISTRATOR")) return errorMessage("`❌` Invalid permissions", interaction);
         if(interaction.options.getSubcommand() == "reset"){
             client.configKV.delete("")
             await interaction.reply({
